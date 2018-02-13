d121623b1265d8331f5b7ea7c143de5ed7992d9a
Markus J. Wetzel <markuswetzel@gmx.net>
2018-02-13 22:32:25 +0100

    Fix bugs

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
 const FUNCTION_NAME = 'defineTranslations';
 
-const MESSAGES = Symbol('IntlizedComponentsMessages');
+const isDefineTranslationsFunction = (path, state) => {
+  const callee = path.get('callee');
+  if (
+    callee.referencesImport('intlized-components', FUNCTION_NAME) ||
+    callee.referencesImport(state.opts.importName, FUNCTION_NAME)
+  ) {
+    return true;
+  }
+
+  return false;
+};
+
+const getKey = path => path.node.name;
+const getValue = path => path.evaluate().value;
 
 export default function () {
   return {
     pre(file) {
-      if (!file.has(MESSAGES)) {
-        file.set(MESSAGES, new Map());
-      }
+      file.set('translations', new Map());
     },
     post(file) {
       // load messages
-      const messages = [...file.get(MESSAGES).values()];
+      const messages = [...file.get('translations').values()];
 
       // save messages in metadata
       // eslint-disable-next-line no-param-reassign
@@ -20,20 +31,20 @@ export default function () {
     visitor: {
       CallExpression(path, state) {
         // check if expression is define translations function
-        if (!path.referencesImport('intlized-components', FUNCTION_NAME)) {
+        if (!isDefineTranslationsFunction(path, state)) {
           return;
         }
 
         // parse messages
         const scope = path.get('arguments')[0];
         const definitions = path.get('arguments')[1];
-        definitions.get('properties').forEach((definition) => {
-          // get messages
-          const messages = state.file.get(MESSAGES);
-          const id = `${scope.get('value')}.${definition.get('key')}`;
-          const defaultMessage = definition.get('value');
 
-          // temporarily store messages
+        // temporarily store messages
+        definitions.get('properties').forEach((definition) => {
+          const messages = state.file.get('translations');
+          const id = `${getValue(scope.get('value'))}.${getKey(definition.get('key'))}`;
+          const defaultMessage = getValue(definition.get('value'));
+
           messages.set(id, {
             id,
             defaultMessage,
